48577926cf49db5d8a131a9fdd0b76d3512616ba
arkwave <ark2210@columbia.edu>
2022-08-06 00:08:07 -0400

    saving

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,6 +15,10 @@ fn main() {
             .read_line(&mut guess) // returns a Result object. references are immutable by default, so &mut returns a mutable reference. 
             .expect("Failed to read line"); // .expect() crashes if Result is of variant Err, returns value if Ok. 
         
+        // note: read_line() call saves the result of read_line to the buffer specified by a mutable reference to <guess>.  mutable refernce
+        // guarantees that this function has exclusive access to the guess variable. 
+    
+        // shadowing: we are "borrowing" the name "guess" from the previously defined str value and replacing it with a u32
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num, 
             Err(_) => continue, // _ is a catch-all value. 
